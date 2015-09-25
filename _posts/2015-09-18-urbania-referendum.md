0ac44bdb0d680b8f2042a803b20211793a1ba012
Erwan d'Orgeville <info@erwandorgeville.com>
2015-09-24 23:03:46 -0400

    Added some wow

---
+++ _posts/2015-09-18-urbania-referendum.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  "Urbania - Referendum"
+order: 3
+image:
+    url: "content/urbania-referendum.JPEG"
+    alt: "Urbania - Referendum"
+---
+
---
+++ js/doge.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    if(location.search == '?doge'){
+        $('.datface img').attr('src', '/img/doge.jpg');
+        $('#wow h1').html('Erwan Dogeville');
+        var dev = $('#wow h2').html().replace('Développeur Web', 'Développeur Wow!');
+        $('#wow h2').html(dev);
+    }
+});
