968068e81b0a79e3611d2beb0047159f233a23b1
Erwan d'Orgeville <info@erwandorgeville.com>
2015-09-24 23:10:08 -0400

    Added some dank memes

--- js/doge.js
+++ js/doge.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     if(location.search == '?doge'){
+        $('body').addClass('doge');
         $('.datface img').attr('src', '/img/doge.jpg');
         $('#wow h1').html('Erwan Dogeville');
         var dev = $('#wow h2').html().replace('Développeur Web', 'Développeur Wow!');
