6f390b8c8c12a2632940e693766b20909d0999cf
Erwan d'Orgeville <info@erwandorgeville.com>
2019-02-25 23:41:43 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,12 +2,9 @@ $(document).ready(function(){
 	$("#fb-like").bind("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function(){
 		$(this).addClass('done');
 	});
-	var readyInterval = setInterval(function(){
-		if($('#fb-like > .fb-like').attr('fb-xfbml-state') == 'rendered'){
-			$('body').addClass('ready');
-			clearInterval(readyInterval);
-		}
-	},100);
+	
+         $('body').addClass('ready');
+
 	$('.print').click(function(){
 		event.preventDefault();
 		window.print();
